8f30cf9e4b61da05298bb06e7dc59c8e783fcd22
Josh Friedlander <josh@kando.eco>
2021-01-03 11:11:52 +0200

    add search_downstream ability, clean up code

--- pinpointing.py
+++ pinpointing.py
@@ -34,28 +34,11 @@ scoring_weights = {
 }
 
 
-def _parser(node, graph):
-    if len(node['children']) == 0:
-        graph.add_node(node['point_id'], name=node['point']['name'])
-        return
-    for child in node['children']:
-        graph.add_edges_from([(node['point_id'], child['point_id'])],
-                             distance=child['parent_distance'])
-        _parser(child, graph)
-
-
-def get_graph(point):
-    graph_data = client.network_graph(point)
-    graph = nx.DiGraph()
-    _parser(graph_data, graph)
-    return graph
-
-
 def _get_data(point, start, end):
     # convert time to epoch, fetch from API, turn into dataframe, return
-    return_data = client.get_data(point_id=point,
-                                  start=start.timestamp(),
-                                  end=end.timestamp())
+    return_data = client.get_all(point_id=point,
+                                 start=start.timestamp(),
+                                 end=end.timestamp())
     samples = return_data['samplings']
     if len(samples) == 0:
         logger.info(f'No data at all for node {point}, continuing the search')
@@ -118,49 +101,108 @@ class Chain:
 
 
 class Pinpointer:
-    def __init__(self, root, graph, query, threshold):
-        self.root = root
-        self.graph = graph
-        self.threshold = threshold
-        self.suspects = []
+    def __init__(self, params):
         self.scores = {}
-        self.max_PI = query['PI'].max()
+        self.suspects = []
+        self.attrs = {}
+        self.ancestors = []
+        self.root = params['root']
+        self.graph = self.get_graph(params['root'])
+        self.threshold = params['threshold']
+        self.device_dict = nx.get_node_attributes(self.graph, 'has_device')
+        self.max_PI = params['query']['PI'].max()
 
-        query = query[scoring_weights].copy()  # drop what's not in weights
+        # drop what's not in weights
+        query = params['query'][set(scoring_weights).intersection(
+            params['query'].columns)].copy()
         for param in query:
             if query[param].dtype == 'float64':
                 if not all(query[param].isna()):
                     query[param] = _normalise(query[param])
 
         self.query = query
+        if params['sensor'] is not None:
+            self.event_sensor = params['sensor']
 
-    def _get_time_shifted_data(self, node, path):
+    def _parser(self, node, graph):
+        if len(node['children']) == 0:
+            graph.add_node(node['point_id'])
+            return
+        for child in node['children']:
+            graph.add_edges_from([(node['point_id'], child['point_id'])],
+                                 weight=child['parent_distance'])
+            self._parser(child, graph)
+
+    def get_graph(self, point):
+        graph_data = client.network_graph(point)
+        graph = nx.DiGraph()
+        self._parser(graph_data, graph)
+
+        self._parse_response(graph_data)
+        nx.set_node_attributes(graph, self.attrs)
+        ancestors = [
+            x['point']['id'] for x in graph_data['ancestors']
+            if x['point']['device']['unit_id'] is not None
+        ]
+        distances = [graph_data['parent_distance']] + [
+            x['parent_distance'] for x in graph_data['ancestors']
+            if x['point']['device']['unit_id'] is not None
+        ][:-1]
+        self.ancestors = list(zip(ancestors, distances))
+        return graph
+
+    def _parse_response(self, response):
+        """
+        Parse API graph structure for "name" and "has_device"
+        :param response:
+        :return: nothing, but populates self.attrs with data
+        """
+        self.attrs[response['point_id']] = {
+            'name': response['point']['name'],
+            'has_device': response['point']['device']['unit_id'] is not None
+        }
+        if len(response['children']) > 0:
+            for ch in response['children']:
+                self._parse_response(ch)
+
+    def _get_time_shifted_data(self, node, path=None, time_difference=None):
         """
         Given a chain and a node, and (start,end) times, find the equivalent time-shifted (start,end) for the node
         Note that we "pad" the time-shifting by 50% each way
         So for example if event takes place at 08:00-08:30 at event node, and candidate node is 3 hours behind,
         we will return data from start - (distance * 1.5), ie 03:30 until end - (distance * 0.5) ie 07:00
         Within this 3.5 hour space we will search for a pattern matching the half-hour event
+        This example is for the common (upstream) case. When downstream we shift time forward.
         node : int
         path : list: chain from event node to node before candidate
         returns: data : time-shifted chain of data from candidate node, expanded by 50% on each side
         """
-        time_difference = self._get_path_distance(path)
-        start = self.query.index.min() - dt.timedelta(minutes=time_difference *
-                                                      1.5)
-        end = self.query.index.max() - dt.timedelta(minutes=time_difference *
-                                                    0.5)
+        if path is None:
+            assert time_difference is not None, "Either path or time_distance must be provided"
+            start = self.query.index.min() + dt.timedelta(
+                minutes=time_difference * 0.5)
+            end = self.query.index.max() + dt.timedelta(
+                minutes=time_difference * 1.5)
+
+        else:
+            time_difference = self._get_path_distance(path)
+            start = self.query.index.min() - dt.timedelta(
+                minutes=time_difference * 1.5)
+            end = self.query.index.max() - dt.timedelta(
+                minutes=time_difference * 0.5)
         data = _get_data(node, start, end)
         return data
 
     def _get_path_distance(self, path):
-        return sum(self.graph[a][b]['distance']
+        return sum(self.graph[a][b]['weight']
                    for (a, b) in zip(path[:-1], path[1:]))
 
     def _check_node(self, node, path):
         if node == self.root:  # no need to check root
             return True, None
-        node_data = self._get_time_shifted_data(node, path)
+        if not self.device_dict[node]:
+            return True, None
+        node_data = self._get_time_shifted_data(node, path=path)
         if node_data is None:  # ie missing data
             if (pd.Timestamp.now(pytz.FixedOffset(120)) -
                     self.query.index.max()).total_seconds() / 60 / 60 < 8:
@@ -180,9 +222,9 @@ class Pinpointer:
         # TODO look for matches historically and returning them to database
         # and then add bank check to pinpointing func
         # TODO receive on which parameter alert was raised, and give that more weight
-        if node_data['PI'].max() < self.max_PI:
-            logger.info('node rejected because of lower PI')
-            return False, None
+        # if node_data['PI'].max() < self.max_PI:
+        #     logger.info('node rejected because of lower PI')
+        #     return False, None
         errors = {}
         # intersection of root_data columns and node columns
         for parameter in set(
@@ -211,7 +253,28 @@ class Pinpointer:
                 return True, errors
         return False, None
 
-    def depth_first_check(self, node, path):
+    def search_downstream(self):
+        distance = 0
+        for node, node_dist in self.ancestors:
+            distance += node_dist
+            logger.info(f'checking downstream from {self.root} to {node}')
+            node_data = self._get_time_shifted_data(node,
+                                                    time_difference=distance)
+            if node_data is None:  # ie missing data
+                if (pd.Timestamp.now(pytz.FixedOffset(120)) -
+                        self.query.index.max()).total_seconds() / 60 / 60 < 8:
+                    logger.warning(
+                        'Under 8h elapsed since event end, data may not be final'
+                    )
+                continue
+                # TODO talk to Naama about waiting for data
+            is_suspect, score = self.compare_data(node_data)
+            if not is_suspect:
+                return
+            self.suspects.append((node, score))
+        return self.suspects
+
+    def search_upstream(self, node, path):
         """
         DFS of tree from root. If node is suspect, keep searching children until end of chain
         :param node: node currently checking
@@ -223,7 +286,7 @@ class Pinpointer:
         if not is_suspect:  # if this isn't a suspect, append path from root to parent
             return False
         if True not in [
-                self.depth_first_check(child, path + [node])
+                self.search_upstream(child, path + [node])
                 for child in self.graph.successors(node)
         ]:
             self.suspects.append(Chain(path + [node]))
@@ -252,21 +315,24 @@ class Pinpointer:
         ]  # remove duplicates
 
 
-def pinpoint(root, query, threshold):
+def pinpoint(params, direction='up'):
     """
-    :param root: int,
-    :param query: a time sequence from a given node, with an area of interest starting at index until (index + duration)
+    param is a dict containing:
+    root: int,
+    query: a time sequence from a given node, with an area of interest starting at index until (index + duration)
     threshold of how far (DTW distance) we will consider a suspect
-    :param threshold: DTW distance allowed for any one sensor
+    threshold: DTW distance allowed for any one sensor
+    sensor: sensor for which the event was flagged
     :return: list of chains of suspects
     """
     # if query in bank:
     #     return 'query already exists in bank'
-    # # TODO bank should use multidim DTW to check if signature already seen
     # bank.append(query)
-    graph = get_graph(root)
-    pinpointer = Pinpointer(root, graph, query, threshold)
-    pinpointer.depth_first_check(root, [])
+    pinpointer = Pinpointer(params)
+    if direction == 'down':
+        return pinpointer.search_downstream()
+
+    pinpointer.search_upstream(params['root'], [])
     pinpointer.clean_up_chains()
     return [
         x.get_score(pinpointer.scores, pinpointer.threshold)
@@ -275,9 +341,15 @@ def pinpoint(root, query, threshold):
 
 
 if __name__ == '__main__':
-    # root_node = 3316  # 3178
-    root_node = 1012
+    # root_node = 3316
+    root_node = 1191
     # orig_query = pd.read_csv('yoke.csv', index_col=0)
     orig_query = pd.read_csv('soreq.csv', index_col=0)
     orig_query.index = pd.to_datetime(orig_query.index)
-    print(pinpoint(root_node, orig_query, threshold=0.06))
+    pinpoint_params = {
+        'root': root_node,
+        'query': orig_query,
+        'threshold': 0.35,
+        'sensor': 'PI'
+    }
+    print(pinpoint(pinpoint_params, 'down'))
--- spec/pinpointing.md
+++ spec/pinpointing.md
@@ -10,11 +10,11 @@ There are various ways to measure distance between two vectors. The naive way is
 
 ![img](eucl.png)
 
-Another method is the *Fréchet distance*: the length of the shortest straight line that would allow both points to be traversed by two connected agents. (The common example is &ldquo;the length of the leash that would let you walk your dog&rdquo;.)
+Another method is the *Fréchet distance*: the length of the shortest straight line that would allow both points to be traversed by two connected agents. (The common example is &ldquo;the length of the leash that would let a person walk a dog&rdquo;.)
 
 ![img](frechet.png)
 
-But a problem that arises with this is what if your curves are not exactly aligned?
+But what if your curves are not exactly aligned? Sometimes the two appear very similar, but are out of phase, or offset in time.
 
 ![img](sin_cos.png)
 
@@ -24,7 +24,7 @@ To solve this we use a method called **Dynamic Time Warping** (DTW). It allows f
 
 ## Finding paths
 
-We iterate recursively over all the child nodes in the grid. At each point, we consider its sensor which are present in the query, and calculate the (z-normalised) DTW distance. A threshold is used to decide relevance (0.35 is the default). 
+We iterate recursively over all the child nodes in the grid. At each point, we consider its sensor which are present in the query, and calculate the (z-normalised) DTW distance. A threshold is used to decide relevance (0.06 is the default). 
 
 If a node has no sensors whose distance is below the threshold, we consider it a dead end and stop looking.
 
@@ -34,7 +34,7 @@ If it has at least one sensor with data, we include it and store its distances.
 
 ## Scores
 
-The distances which are below the threshold are first weighted based on sensor type (this can increase the distance to be greater than the threshold). The distance is converted to a percentage of confidence with the following formula (where $\theta$ is the threshold): $ \frac{\theta - \min{(distance, \theta)}}{\theta}$
+The distances which are below the threshold are first weighted based on sensor type (this can increase the distance to be greater than the threshold). The distance is converted to a percentage of confidence with the formula: $ \frac{threshold - distance}{threshold}$
 
 ## Chains
 
@@ -42,18 +42,23 @@ Ultimately a chain has the following structure:
 
 ```python
 [
-    ROOT, {
-        NODE_1: {
-            EC: 81%,
-            PH: 12%
-        },
-        NODE_2: Missing data,
-        NODE_3: {
-            PI: 46%,
-            TEMPERATURE: 12%,
-            ORP: 6%
-        }
-    }
+  ROOT,
+  NODE_1: {
+    EC: 81%,
+    PH: 12%
+  },
+  NODE_2: none,
+  NODE_3: {
+    PI: 46%,
+    TEMPERATURE: 12%,
+    ORP: 6%
+  }
 ]
 ```
 
+To get the score for a chain, we can take the mean of each node's max confidence level. 
+
+## Open questions
+
+- If we are given an "event parameter" (e.g. this is an EC event), we can weight that parameter as being worth more. But are we still interested if we only find matches on other sensors?
+- Should we just return one match, or several? Or should we have a confidence level above which we will return a match as definitely correct?
